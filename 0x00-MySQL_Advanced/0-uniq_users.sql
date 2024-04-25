be3e9290aa04348386fd8d14d1116b31d33faa0c
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-04-25 18:00:41 +0100

    Create users table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- A script that creates a table `users`
+-- The script does not fail if the table already exists
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
+);
