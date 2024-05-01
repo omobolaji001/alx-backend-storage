7959120e9004c99c0f0e5922f55c72514dbce7cf
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-01 02:45:18 +0000

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -18,8 +18,6 @@ if __name__ == "__main__":
     delete_logs = collection.count_documents({"method": "DELETE"})
     checks = collection.count_documents({"method": "GET", "path": "/status"})
 
-    print("{} logs\n Methods:\n \t  method GET: {} \n\t  method POST: {} \n\t"
-          "  method PUT: {} \n\t  method PATCH: {} \n\t  method DELETE: {}"
-          "\n status_checks: {}".format(all_logs, get_logs, post_logs,
-                                            put_logs, patch_logs, delete_logs,
-                                            checks))
+    print("{} logs\n Methods:\n \t  method GET: {} \n\t  method POST: {} \n\t  method"
+              " PUT: {} \n\t  method PATCH: {} \n\t  method DELETE: {}"
+              "\n status_checks: {}".format(all_logs, get_logs, post_logs, put_logs, patch_logs, delete_logs, checks))
